1427bd82f06693661a2658f57b2d5958e34d17ea
Keyz <praveenpaulraj59@gmail.com>
2022-09-01 22:44:11 +0530

    updated overall code

--- mobilevalidation.java
+++ mobilevalidation.java
@@ -2,17 +2,17 @@ import java.util.Scanner ;
 
 public class Main
 {
-    public static void main ( String [ ] args )
+    public static void main(String[] args)
     {
-        Scanner sc = new Scanner ( System.in ) ;
-        String s1 = sc.nextLine ( ) ;
-        if ( s1.matches ( "[0-9]{3}[-]{1}[0-9]{3}[-]{1}[0-9]{4}"))
+        Scanner sc = new Scanner(System.in) ;
+        String s1 = sc.nextLine();
+        if(s1.matches("[0-9]{3}[-]{1}[0-9]{3}[-]{1}[0-9]{4}"))
         {
-            System.out.println ( " Number  is valid " );
+            System.out.println("Number  is valid");
          }
         else
             {
-          System.out.println ( " Number is invalid ");
+          System.out.println("Number is invalid");
             }
     }
 }
